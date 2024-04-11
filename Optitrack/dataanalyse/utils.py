30ec84e4f12f2e1c0e675f691cdb3cc984964632
TacYee <450356982@qq.com>
2024-04-11 09:34:27 +0200

    update LSTM and 5 test with mean and variance

--- Optitrack/dataanalyse/utils.py
+++ Optitrack/dataanalyse/utils.py
@@ -149,23 +149,28 @@ def load_data(data_folder):
 
 #         return input_data_batch, target_data_batch
         
-def dataset( data, targets, sequence_length):
+def dataset( data, targets, sequence_length, trial_sizes):
         data = data
         targets = targets
         sequence_length = sequence_length
+        trial_sizes = trial_sizes
         output_pair=[]
-        start_idx = 0
-        end_idx = len(targets)
-        for i in range(start_idx, end_idx, 1):
-            if i-sequence_length+1 < 0:
-                input_data = np.concatenate((data[i-sequence_length+1:], data[:i + 1])) 
-            else:
-                input_data = data[i - sequence_length + 1 : i + 1]
-            target_data = targets[i]
-            input_data = torch.tensor(input_data, dtype=torch.float32)
-            target_data = torch.tensor(target_data, dtype=torch.float32)
-            output_pair.append((input_data,target_data))
-        return output_pair  
+        for i in range (len(trial_sizes)):
+            trial_size = trial_sizes[i]
+            start_idx = sum(trial_sizes[:i])
+            end_idx = start_idx + trial_size
+            for j in range(start_idx, end_idx, 1):
+                if j -start_idx -sequence_length+1 < 0:
+                    num_zeros = start_idx + sequence_length - j - 1
+                    input_data = np.pad(data[start_idx:j+1], ((num_zeros, 0), (0, 0)), 'constant')
+                else:
+                    input_data = data[j - sequence_length + 1 : j + 1]
+                target_data = targets[j]
+                input_data = torch.tensor(input_data, dtype=torch.float32)
+                target_data = torch.tensor(target_data, dtype=torch.float32)
+                output_pair.append((input_data,target_data))
+        return output_pair   
+
 
 def save_output_image(model, data_loader, laser, output_file):
     model.eval()
@@ -226,7 +231,7 @@ def save_deviation_image(model, data_loader, laser, output_file):
     plt.savefig(f"{output_file}")
     plt.close()
 
-def save_reconstruction_surface(model, data_loader, orientation, position, laser, output_file):
+def save_reconstruction_surface_1(model, data_loader, orientation, position, laser, output_file):
     model.eval()
     all_outputs = []
     all_labels = []
@@ -257,15 +262,105 @@ def save_reconstruction_surface(model, data_loader, orientation, position, laser
     Whisker_y = (218 - all_outputs +16) * np.cos(np.radians(orientation)) + position_Y
     # output the deviation
     plt.figure(figsize=(10, 6))
-    plt.scatter(GT_x, GT_y,s=0.1,c='black', label='GT')
-    plt.scatter(position_X , position_Y ,s=1, label='position')
-    plt.scatter(Laser_x, Laser_y,s=1, label='laser')
-    plt.scatter(Whisker_x, Whisker_y,s=1, label='whisker')
-    plt.title(f"Surface Reconstruction_Whisker")
+    plt.scatter(GT_x[0:151], GT_y[0:151], s=1, c='black', label='GT')
+    plt.scatter(position_X[0:151] , position_Y[0:151] ,s=1, label='position')
+    plt.scatter(Laser_x[0:151], Laser_y[0:151],s=1, label='laser')
+    plt.scatter(Whisker_x[0:151], Whisker_y[0:151],s=1, label='whisker')
+    plt.title(f"Surface Reconstruction_Whisker_trial1")
     plt.legend()
     plt.xlabel('X (mm)')
     plt.ylabel('Y (mm)')
-    plt.savefig(f"{output_file}")
+    plt.savefig(f"{output_file[:-4]}_trial1{output_file[-4:]}")
+    plt.close()
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(GT_x[151:305], GT_y[151:305], s=1, c='black', label='GT')
+    plt.scatter(position_X[151:305] , position_Y[151:305] ,s=1, label='position')
+    plt.scatter(Laser_x[151:305], Laser_y[151:305],s=1, label='laser')
+    plt.scatter(Whisker_x[151:305], Whisker_y[151:305],s=1, label='whisker')
+    plt.title(f"Surface Reconstruction_Whisker_trial2")
+    plt.legend()
+    plt.xlabel('X (mm)')
+    plt.ylabel('Y (mm)')
+    plt.savefig(f"{output_file[:-4]}_trial2{output_file[-4:]}")
+    plt.close()
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(GT_x[305:445], GT_y[305:445], s=1, c='black', label='GT')
+    plt.scatter(position_X[305:445] , position_Y[305:445] ,s=1, label='position')
+    plt.scatter(Laser_x[305:445], Laser_y[305:445],s=1, label='laser')
+    plt.scatter(Whisker_x[305:445], Whisker_y[305:445],s=1, label='whisker')
+    plt.title(f"Surface Reconstruction_Whisker_trial3")
+    plt.legend()
+    plt.xlabel('X (mm)')
+    plt.ylabel('Y (mm)')
+    plt.savefig(f"{output_file[:-4]}_trial3{output_file[-4:]}")
+    plt.close()
+
+def save_reconstruction_surface_2(model, data_loader, orientation, position, laser, output_file):
+    model.eval()
+    all_outputs = []
+    all_labels = []
+    with torch.no_grad():
+        for inputs, labels in data_loader:
+            inputs = inputs.to(device)
+
+            outputs= model(inputs)
+
+            # 将输出和真实标签保存到列表中
+            all_outputs.append(outputs.cpu().numpy())
+            all_labels.append(labels.cpu().numpy())
+
+    # 将列表转换为 numpy 数组
+    all_outputs = np.concatenate(all_outputs, axis=0).flatten()
+    all_labels = np.concatenate(all_labels, axis=0).flatten()
+
+    position_X = position[:,1] * 1000
+    position_Y = position[:,0] * 1000
+
+    GT_x = -(218 - all_labels + 16) * np.sin(np.radians(orientation)) + position_X
+    GT_y = (218 - all_labels + 16) * np.cos(np.radians(orientation)) + position_Y
+
+    Laser_x = -(218 - laser + 16) * np.sin(np.radians(orientation)) + position_X
+    Laser_y = (218 - laser + 16) * np.cos(np.radians(orientation)) + position_Y
+    
+    Whisker_x = -(218 - all_outputs +16) * np.sin(np.radians(orientation)) + position_X
+    Whisker_y = (218 - all_outputs +16) * np.cos(np.radians(orientation)) + position_Y
+    # output the deviation
+    plt.figure(figsize=(10, 6))
+    plt.scatter(GT_x[0:151], GT_y[0:151], s=1, c='black', label='GT')
+    plt.scatter(position_X[0:151] , position_Y[0:151] ,s=1, label='position')
+    plt.scatter(Laser_x[0:151], Laser_y[0:151],s=1, label='laser')
+    plt.scatter(Whisker_x[0:151], Whisker_y[0:151],s=1, label='whisker')
+    plt.title(f"Surface Reconstruction_Whisker_trial1")
+    plt.legend()
+    plt.xlabel('X (mm)')
+    plt.ylabel('Y (mm)')
+    plt.savefig(f"{output_file[:-4]}_trial1{output_file[-4:]}")
+    plt.close()
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(GT_x[151:314], GT_y[151:314], s=1, c='black', label='GT')
+    plt.scatter(position_X[151:314] , position_Y[151:314] ,s=1, label='position')
+    plt.scatter(Laser_x[151:314], Laser_y[151:314],s=1, label='laser')
+    plt.scatter(Whisker_x[151:314], Whisker_y[151:314],s=1, label='whisker')
+    plt.title(f"Surface Reconstruction_Whisker_trial2")
+    plt.legend()
+    plt.xlabel('X (mm)')
+    plt.ylabel('Y (mm)')
+    plt.savefig(f"{output_file[:-4]}_trial2{output_file[-4:]}")
+    plt.close()
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(GT_x[314:457], GT_y[314:457], s=1, c='black', label='GT')
+    plt.scatter(position_X[314:457] , position_Y[314:457] ,s=1, label='position')
+    plt.scatter(Laser_x[314:457], Laser_y[314:457],s=1, label='laser')
+    plt.scatter(Whisker_x[314:457], Whisker_y[314:457],s=1, label='whisker')
+    plt.title(f"Surface Reconstruction_Whisker_trial3")
+    plt.legend()
+    plt.xlabel('X (mm)')
+    plt.ylabel('Y (mm)')
+    plt.savefig(f"{output_file[:-4]}_trial3{output_file[-4:]}")
     plt.close()
 
     
